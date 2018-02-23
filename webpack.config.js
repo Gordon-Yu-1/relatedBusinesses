cf0f6bd3dd4c6dc96535850554bc7c2485c15177
Andrea <ellisona@gmail.com>
2018-02-23 12:33:32 -0800

    found error in eslintrc file which impacted overriding of tab and indentation. Corrected.

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,23 @@ const appDir = path.resolve(__dirname, 'client');
 const buildDir = path.resolve(__dirname, 'public');
 
 module.exports = {
-	entry: `${appDir}/index.jsx`,
-	output: {
-		path: buildDir,
-		filename: 'bundle.js',
-	},
-	module: {
-	    rules: [
-    	{
-	    		test: /\.(js|jsx)$/,
-	    		exclude: /node_modules/,
-	    		use: ['babel-loader'],
-    	},
-    	{
-	    		test: /\.js$/,
-	    		exclude: /node_modules/,
-	    		use: ['babel-loader', 'eslint-loader'],
-    	},
-	    ],
-	},
+  entry: `${appDir}/index.jsx`,
+  output: {
+    path: buildDir,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
 };
