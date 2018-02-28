dee0fa6f01920678b7f8870a32d5a4d1a1cad39a
Andrea <ellisona@gmail.com>
2018-02-27 19:50:11 -0800

    Added start of Schema in mongo for related businesses

--- db/seed_db.js
+++ db/seed_db.js
@@ -8,47 +8,16 @@ mongoose.connect('mongodb://localhost/related');
 
 const relatedBizSchema = mongoose.Schema({
   id: Number,
-  name: String,
-  url: String,
-  description: String,
-  created: Date,
-  updated: Date,
-
-  userid: Number,
-  username: String,
-  useravatar: String,
-  userprofile: String,
+  businessName: String,
+  firstImage: String,
+  latitude: Number,
+  longitude: Number,
+  avgRating: Number,
+  quantityRatings: Number,
+  fullReview: String,
+  oneLineReview: String,
+  metatags: Array, // double check this syntax
+  listsWithThisBiz: Array, // double check this syntax
 });
 
-// let Repo = mongoose.model('Repo', repoSchema);
-
-// let save = repos => {
-//     repos.forEach(repo => {
-//       Repo.find({id: repo.id})
-//       .then(docs => {
-//         if (docs.length === 0) {
-//           Repo.create(repo);
-//         } 
-//       })
-//       .catch(error => console.error(error));
-//     });
-// };
-
-// let retrieve = (num = 25, sortBy = 'updated', direction = -1) => {
-//   if (([-1, 'desc', 'descending']).indexOf(direction) !== -1) {
-//     sortBy = `-${sortBy}`;
-//   }
-//   return new Promise((resolve, reject) => {
-//     Repo.find()
-//       .sort(sortBy)
-//       .limit(num)
-//       .exec()
-//     .then(results => resolve(results))
-//     .catch(error => reject(error));
-//   });
-// }
-
-// console.log('made the schema');
-
-// module.exports.save = save;
-// module.exports.retrieve = retrieve;
+:
