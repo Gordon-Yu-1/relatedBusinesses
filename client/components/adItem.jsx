3af7349b45439921f8d658a62c6d8fa37c87c779
Andrea <ellisona@gmail.com>
2018-03-08 11:05:53 -0800

    Communication established to render components properly.

--- client/components/adItem.jsx
+++ client/components/adItem.jsx
@@ -6,7 +6,7 @@ const AdItem = props => (
   <div>
     <div>
       <span className={style.ad}>Ad</span><span className={style.bizTitle}>{props.biz.businessName}</span>
-      <img className={style.bizPic} src={props.biz.firstImage} alt="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/fe8c0c8725d3/assets/img/default_avatars/business_90_square.png" />
+      <img className={style.bizPic} src={props.biz.firstImage} alt={props.biz.firstImage} />
       <div>
         <img className={style.rating} src="http://images.nymag.com/news/intelligencer/intelposts120326_starstruck_560.jpg" alt="http://images.nymag.com/news/intelligencer/intelposts120326_starstruck_560.jpg"/><span className={style.rating}>{props.biz.quantityRatings} reviews</span>
       </div>
