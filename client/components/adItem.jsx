907fbd958576b72beb8451909237c51860b51a35
Andrea <ellisona@gmail.com>
2018-03-08 10:43:37 -0800

    Updtaed default images

--- client/components/adItem.jsx
+++ client/components/adItem.jsx
@@ -6,7 +6,7 @@ const AdItem = props => (
   <div>
     <div>
       <span className={style.ad}>Ad</span><span className={style.bizTitle}>{props.biz.businessName}</span>
-      <img className={style.bizPic} src={props.biz.firstImage} alt="https://rlv.zcache.com/happy_smiley_face_round_stickers-rbdcd90a58b8e40a9b895e7c2fd1e65ef_v9waf_8byvr_540.jpg" />
+      <img className={style.bizPic} src={props.biz.firstImage} alt="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/fe8c0c8725d3/assets/img/default_avatars/business_90_square.png" />
       <div>
         <img className={style.rating} src="http://images.nymag.com/news/intelligencer/intelposts120326_starstruck_560.jpg" alt="http://images.nymag.com/news/intelligencer/intelposts120326_starstruck_560.jpg"/><span className={style.rating}>{props.biz.quantityRatings} reviews</span>
       </div>
