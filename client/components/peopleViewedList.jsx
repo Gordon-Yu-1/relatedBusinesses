0e9e32eb8b93f96888c133425191a126282e4e20
Andrea <ellisona@gmail.com>
2018-03-08 16:09:13 -0800

    Restructuring of file system to accomodate rendering into different ids on DOM

--- client/components/peopleViewedList.jsx
+++ client/components/peopleViewedList.jsx
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 import NonAdItem from './nonAdItem';
 import style from './styles/adList.css';
 
-const PeopleViewed = props => (
+const PeopleViewedList = props => (
   <div className={style.adList}>
     { props.businesses.map(business => <li className={style.listEntry}><NonAdItem biz={business} /></li>)}
   </div>
 );
 
-PeopleViewed.propTypes = {
+PeopleViewedList.propTypes = {
   businesses: PropTypes.arrayOf(PropTypes.object),
 };
 
-PeopleViewed.defaultProps = {
+PeopleViewedList.defaultProps = {
   businesses: [],
 };
 
-export default PeopleViewed;
+export default PeopleViewedList;
--- client/index.jsx
+++ client/index.jsx
@@ -1,70 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import url from 'url';
-import axios from 'axios';
-import style from './components/styles/adContent.css';
-import AdSideList from './components/adSideList';
-import AdCenterList from './components/adCenterList';
-import PeopleViewed from './components/peopleViewed';
-import dummy from '../db/Sues-fake-data';
+import AdCenter from './adCenter';
+import AdSide from './adSide';
+import PeopleViewed from './peopleViewed';
 
-class Ad extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      relatedBizs: dummy,
-      currentBiz: 1, // the id of the product detail page's currently featured biz
-    };
-    // console.log('this is inside the constructed');
-  }
-
-  componentWillMount() {
-    const thisUrl = document.location;
-    const path = thisUrl.pathname;
-    // console.log('Path', path); // should result in /biz/SOME_ID
-    const reqId = path.split('/')[2]; 
-    // console.log('reqId, ', reqId);// should result in the ID
-    this.state.currentBiz = reqId;
-    axios.get(`http://127.0.0.1:9002/related/${reqId}/`)
-      .then((res) => {
-        this.setState({ relatedBizs: res.data });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <div className={style.allContent}>
-        <div>
-          <span className={style.adType}>Recommended Businesses for </span>
-          <span className={style.businessName}>Business Id {this.state.currentBiz}</span>
-        </div>
-        <div className={style.adType}>Center Top Ads</div>
-        <ul className={style.businessName}>
-          <AdCenterList businesses={this.state.relatedBizs.slice(0, 2)} />
-        </ul>
-        <div className={style.adType}>SideBar Ads</div>
-        <ul className={style.businessName}>
-          <AdSideList businesses={this.state.relatedBizs.slice(2, 4)} />
-        </ul>
-        <div className={style.adType}>People Also Viewed</div>
-        <ul className={style.businessName}>
-          <PeopleViewed businesses={this.state.relatedBizs.slice(4, 6)} />
-        </ul>
-      </div>
-    );
-  }
-}
-
-Ad.propTypes = {
-  currentBiz: PropTypes.number,
-};
-
-Ad.defaultProps = {
-  currentBiz: 0,
-};
-
-ReactDOM.render(<Ad />, document.getElementById('ad'));
+ReactDOM.render(<AdCenter />, document.getElementById('ad-top'));
+ReactDOM.render(<AdSide />, document.getElementById('ad-side'));
+ReactDOM.render(<PeopleViewed />, document.getElementById('people-viewed'));
