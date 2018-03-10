b703991067254e820a66309d837fe428b988146b
Andrea <ellisona@gmail.com>
2018-03-10 11:48:43 -0800

    refactored html to ensure that ad components are not blocked by Chrome

--- client/index.jsx
+++ client/index.jsx
@@ -4,6 +4,6 @@ import AdCenter from './adCenter';
 import AdSide from './adSide';
 import PeopleViewed from './peopleViewed';
 
-ReactDOM.render(<AdSide />, document.getElementById('ad-side'));
+ReactDOM.render(<AdSide />, document.getElementById('related-side'));
 ReactDOM.render(<PeopleViewed />, document.getElementById('people-viewed'));
-ReactDOM.render(<AdCenter />, document.getElementById('ad-top'));
+ReactDOM.render(<AdCenter />, document.getElementById('related-top'));
